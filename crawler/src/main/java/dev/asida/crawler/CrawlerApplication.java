a0dffde94d08f38c0e86ee81d34bd54c96480a7e
Asida Yambay <asida.yambay@gmail.com>
2022-03-01 11:01:47 +0100

    Create project structure
    Changes:
    - Create a maven parent project with two child modules, that can be build separately.
    - Create mvn project for Crawler using Spring initializer together with dependencies: dev tools, h2, spring data jpa.
    - Create mvn project for Service using Spring initializer together with dependencies: dev tools, h2, spring data jpa, spring web.
    
    Closes #2

---
+++ crawler/src/main/java/dev/asida/crawler/CrawlerApplication.java
@@ -0,0 +1,13 @@
+package dev.asida.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerApplication.class, args);
+	}
+
+}
