ca88233a71e3d0e7b03db078c40510d8ec11b07a
Asida Yambay <asida.yambay@gmail.com>
2022-03-16 11:18:03 +0100

    Implement crawler.
    Changes:
    - Create central Repository interface
    - Add @Entity functionality to the Articles
    - Create a test h2 database in memory
    - Move test DB to the test/resources folder

--- crawler/src/main/java/dev/asida/crawler/json/Article.java
+++ crawler/src/main/java/dev/asida/crawler/json/Article.java
@@ -1,50 +1,153 @@
 package dev.asida.crawler.json;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.time.LocalDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 /**
  * Class is a Spring Component that is used for serializing a one news article.
+ * It is also used as entity by Spring Data to map domain classes to the DB tables.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Entity
+@Table(name = "articles")
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String author;
     private String title;
     private String description;
     private String url;
-    private LocalDate publishedAt;
+    private String urlToImage;
+    private LocalDateTime publishedAt;
+    private String content;
+
+    // Default constructor is required for JPA
+    public Article() {
+    }
+
+    public Article(Integer id, String author, String title, String description,
+                   String url, String urlToImage, LocalDateTime publishedAt, String content) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public Article(String author, String title, String description,
+                   String url, String urlToImage, LocalDateTime publishedAt, String content) {
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public String getUrl() {
         return url;
     }
-    public LocalDate getPublishedAt() {
+
+    public LocalDateTime getPublishedAt() {
         return publishedAt;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public void setUrl(String url) {
         this.url = url;
     }
-    public void setPublishedAt(LocalDate publishedAt) {
+
+    public void setPublishedAt(LocalDateTime publishedAt) {
         this.publishedAt = publishedAt;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     @Override
     public String toString() {
         return "Article{" +
-                "title='" + title + '\'' +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
                 ", description='" + description + '\'' +
                 ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
                 ", publishedAt=" + publishedAt +
+                ", content='" + content + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id) &&
+                Objects.equals(author, article.author) &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(description, article.description) &&
+                Objects.equals(url, article.url) &&
+                Objects.equals(urlToImage, article.urlToImage) &&
+                Objects.equals(publishedAt, article.publishedAt) &&
+                Objects.equals(content, article.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, author, title, description, url, urlToImage, publishedAt, content);
     }
 }
